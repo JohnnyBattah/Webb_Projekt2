3625c3ebd21bb3ab348044652ddd62da0c5b07dd
Johnny Battah <johnny.battah@elev.ga.ntig.se>
2025-02-11 18:51:12 +0100

    Börjat med JS - klar med metoden signera

---
+++ script.js
@@ -0,0 +1,6 @@
+function signera(){
+    let email = document.querySelector("#email").value;
+    alert("Tack för att du anmält dig till vårt nyhetsbrev!");
+    document.querySelector("#email").value = "";
+}
+
