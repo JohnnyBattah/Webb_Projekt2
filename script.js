aeddaaa3e10f8d566f981a70d75b67bf9be07902
Johnny Battah <johnny.battah@elev.ga.ntig.se>
2025-02-27 18:17:01 +0100

    Varukorg kan öppnas och stängas

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ function signera(){
     document.querySelector("#email").value = "";
 }
 
+let varukorgContainer = document.querySelector("#varukorg-container");
+
+function visaVarukorg() {
+    varukorgContainer.classList.add("open"); // Lägg till klassen "open"
+}
+
+function döljVarukorg() {
+    varukorgContainer.classList.remove("open"); // Ta bort klassen "open"
+}
